bc01a32bafec4ba38b41015f5d4ae373b818758d
Jingyu Guo <jguo@gatech.edu>
2017-12-29 00:51:47 -0800

    keep working on rnn script

--- rnn.py
+++ rnn.py
@@ -13,25 +13,46 @@ from data_processing import encoding, decoding, reverse_seq
 from vocabulary import TRUE_DNA_CODON_TABLE, NUCLEOTIDE, AMINO_ACID, \
     STOP_CODONS, END_OF_SENTENCE
 
+def padding_vector(v1, v2):
+    """
+    padding the 2d shorter matrix by the end with zeros to be taller
+    :param v1: numpy 2d array
+    :param v2: another numpy 2d array
+    :return: two numpy 2d array with the same length of the original longer one
+    """
+    v1_r, v1_c = np.shape(v1)
+    v2_r, v2_c = np.shape(v2)
+    if v1_r < v2_r:
+        v1 = np.append(v1, np.zeros((v2_r-v1_r, v1_c)), axis = 0)
+    elif v1_r > v2_r:
+        v2 = np.append(v2, np.zeros((v1_r-v2_r, v2_c)), axis = 0)
+    else:
+        pass
+    return v1, v2
 
 
-def cross_entropy_loss(self, Y_hat, Y):
+def cross_entropy_correct_prob(Y_hat, Y):
     """
     The loss function here is defined as an average over all amino acid
     cross entropy loss (regardless which samples they are from).
     It is good for concatenation of all samples (amino acid string vectors) in a
     batch together.
+    What can be done better is https://distill.pub/2017/ctc/
     :param Y_hat: Y_hat is a collection of y_hat, such as y_hat = decoder(
     encoder(x)). y_hat is a softmax output vector for one amino acid.
     :param Y: Y is a collection of Y. y is also an encoded one hot vector for
     one amino acid.
     :return: loss as defined by categorical entropy
     """
-    loss = 0.0
     # the zipped list will be truncated by the shorter sequence of Y_hat or Y
-    for y_hat, y in zip(Y_hat, Y):
-        loss += -1.0 * np.sum(np.multiply(y, np.log(y_hat)))
-    return loss/len(Y)
+    # so the shorter one will be padded with zero vectors by the end
+    Y_hat, Y = padding_vector(Y_hat, Y)
+    corr_prob = -1.0 * np.sum(np.multiply(Y, np.log(Y_hat)), axis=1)
+    return corr_prob
+
+def total_loss(Y_hat, Y):
+    return np.sum(cross_entropy_correct_prob(Y_hat, Y)) / len(Y)
+
 
 def softmax(x):
     """
@@ -49,7 +70,7 @@ def softmax(x):
 def tanh_derivative(x):
     """
     https://socratic.org/questions/what-is-the-derivative-of-tanh-x
-    :param x: loss or
+    :param x: output matrix
     :return: d_tanh
     """
     return 1.0 - np.tanh(x)**2
@@ -74,7 +95,7 @@ class RNN(object):
         self.o_dim = o_dim
         self.bptt_truncate = bptt_truncate
         self.rnn_role = rnn_role
-        self.time_step = 0
+        self.timesteps = 0
         # param init
         self.W_x = self.xavier_param_init(self.h_dim, self.x_dim, self.x_dim)
         self.W_h = self.xavier_param_init(self.h_dim, self.h_dim, self.h_dim)
@@ -106,15 +127,16 @@ class RNN(object):
         :param X: input -- 2d matrix with each row being an one hot encoding
         :return: tuple of size two (H, S, O)
         """
-        self.time_step = len(X)
+        self.timesteps = len(X)
         # time_step + 1 to add an initial hidden state for the neuron
-        H = np.zeros((self.time_step + 1, self.h_dim))
-        O = np.zeros((self.time_step, self.o_dim))
-        for t in range(self.time_step):
+        H = np.zeros((self.timesteps + 1, self.h_dim))
+        O = np.zeros((self.timesteps, self.o_dim))
+        for t in range(self.timesteps):
             # update
-            H[t] = np.tanh(self.W_h.dot(H[t - 1]) + self.W_x.dot(X[t]))
+            H[t] = np.tanh(np.dot(self.W_h, H[t - 1]) + np.dot(self.W_x, X[t]) +
+                           self.b)
             # the output itself is based on the current timestep hidden status
-            O[t] = softmax(self.W_o.dot(H[t]))
+            O[t] = softmax(np.dot(self.W_o, H[t]) + self.c)
         return H, O
 
     def encoding_forward(self, X):
@@ -135,21 +157,23 @@ class RNN(object):
         """
         H = C
         O = W
-        while O[self.time_step] is not eos_vec:
+        while O[self.timesteps] is not eos_vec:
             # update timestep counts
-            self.time_step += 1
+            self.timesteps += 1
             # making spaceholder
             H = np.append(H, np.zeros((1, self.h_dim)), axis = 0)
             O = np.append(O, np.zeros((1, self.o_dim)), axis = 0)
             # update
             # notice that the input X is the last timestep output
-            H[self.time_step] = np.tanh(self.W_h.dot(H[self.time_step - 1]) +
-                                        self.W_x.dot(O[self.time_step - 1]))
+            H[self.timesteps] = np.tanh(np.dot(self.W_h, H[self.timesteps - 1]) +
+                                        np.dot(self.W_x, O[self.timesteps - 1]) +
+                                        self.b)
             # the output itself is based on the current timestep hidden status
-            O[self.time_step] = softmax(self.W_o.dot(H[self.time_step]))
+            O[self.timesteps] = softmax(np.dot(self.W_o, H[self.timesteps]) +
+                                        self.c)
         return H, O
 
-    def backprop_through_time(self, error, k1):
+    def backprop_through_time(self, O, Y, k1):
         """
         A truncated back prop through timestep method for passing back loss
         gradient though rnn.
@@ -171,55 +195,59 @@ class RNN(object):
 
         In this implementation, the gradient passing back is separated from
         the gradient descent.
+        :param O: normalized output probs
+        :param Y: labels one hotcoded
+        :param k1: timesteps
+        :return: derivatives
         """
-        # implement configuration of TBPTT(k1,k2), where k1<k2<timesteps:
-        # Multiple updates are performed per sequence which can accelerate training.
         # in case k1 (which would be input sequence length or timesteps) is
         # smaller than predefined self.bptt_truncate
+        k1 = self.timesteps
         if k1 < self.bptt_truncate:
             k2 = k1
         else:
             k2 = self.bptt_truncate
 
         # init placeholder for gradients
-        dL_dW_x = np.zeros(self.W_x.shape).astype(np.float32)
-        dL_dW_h = np.zeros(self.W_h.shape).astype(np.float32)
-        dL_dW_o = np.zeros(self.W_o.shape).astype(np.float32)
-        dL_db = np.zeros([self.h_dim]).astype(np.float32)
-        dL_dc = np.zeros([self.o_dim]).astype(np.float32)
+        dW_x = np.zeros(self.W_x.shape).astype(np.float32)
+        dW_h = np.zeros(self.W_h.shape).astype(np.float32)
+        dW_o = np.zeros(self.W_o.shape).astype(np.float32)
+        db = np.zeros(self.h_dim).astype(np.float32)
+        dc = np.zeros(self.o_dim).astype(np.float32)
 
-        delta_o = o
-        delta_o[np.arange(len(y)), y] -= 1.
-        # For each output backwards...
-        for t in np.arange(T)[::-1]:
-            dLdV += np.outer(delta_o[t], s[t].T)
-            # Initial delta calculation
-            delta_t = self.V.T.dot(delta_o[t]) * (1 - (s[t] ** 2))
-            # Backpropagation through time (for at most self.bptt_truncate steps)
-            for bptt_step in np.arange(max(0, t - self.bptt_truncate), t + 1)[
-                             ::-1]:
-                # print "Backpropagation step t=%d bptt step=%d " % (t, bptt_step)
+        O, Y = padding_vector(O, Y)
+        dO = O - Y # only works for one-hot encoding, dO shaped (sample, o_dim)
+        for t in reversed(range(k1)):
+            # for output weights at timestep t
+            # adding time t derivative -- see it as derivative to correct the
+            # particular one input at time t
+            # By adding up, the gradients will accumulate derivatives at all
+            # timesteps, as from all input in the sample sequence
+            dW_o += np.outer(dO[t], self.H[t]) # shaped (o_dim, h_dim)
+            dc += np.sum(dO[t], axis=0, keepdims=True)
+            # for weights for both prev hidden state and input and bias at
+            # time t
+            # dO[t] shaped 
+            dH = np.dot(dO[t], self.W_o[t]) * tanh_derivative(self.H[t])
+            # Backprop through time by max(0, t-k2) steps (see above) using
+            # derivatives at time t
+
+            for t2 in reversed(range(max(0, t - k2), t + 1)):
                 dLdW += np.outer(delta_t, s[bptt_step - 1])
                 dLdU[:, x[bptt_step]] += delta_t
                 # Update delta for next step
                 delta_t = self.W.T.dot(delta_t) * (1 - s[bptt_step - 1] ** 2)
 
-        for i in range(k1) # k1
-        for i in range(bptt_truncate): # k2
-
-
-    def update(self, dL_dW_x, dL_dW_h, dL_dW_o, learning_rate = 0.005):
+    def update(self, dW_x, dW_h, dW_o, db, dc, learning_rate = 0.005):
         """
         update weights by gradient
-        :param dL_dW_x:
-        :param dL_dW_h:
-        :param dL_dW_o:
-        :param learning_rate:
         :return:
         """
-        self.W_x -= learning_rate * dL_dW_x
-        self.W_h -= learning_rate * dL_dW_h
-        self.W_o -= learning_rate * dL_dW_o
+        self.W_x -= learning_rate * dW_x
+        self.W_h -= learning_rate * dW_h
+        self.W_o -= learning_rate * dW_o
+        self.b   -= learning_rate * db
+        self.c   -= learning_rate * dc
 
 
 
@@ -234,6 +262,17 @@ class Seq2Seq(object):
                            bptt_truncate=4, rnn_role='decoder')
         self.time_step # this is the k1 by Ilya Sutskever
 
+    def forward(self, X, stop_vec):
+        """
+        an encoder-decoder forward
+        :param X: input sample as an encoding of a sequence
+        :return: ec_H, ec_O, dc_H, dc_O
+        """
+        ec_H, ec_O = self.encoder.encoding_forward(X)
+        dc_H, dc_O = self.decoder.decoding_forward(ec_H[-1],
+                                                   ec_O[-1],
+                                                   stop_vec)
+        return ec_H, ec_O, dc_H, dc_O
 
     def train(self, data, learning_rate=0.005, epoch=100,
               reverse_input=False, stop_vec=encoding(END_OF_SENTENCE, AMINO_ACID)):
@@ -241,13 +280,11 @@ class Seq2Seq(object):
             X, Y = data[i]
             if reverse_input: X = reverse_seq(X)
             # forward
-            ec_H, ec_O = self.encoder.encoding_forward(X)
-            dc_H, dc_O = self.decoder.decoding_forward(ec_H[-1],
-                                                       ec_O[-1],
-                                                       stop_vec)
+            ec_H, ec_O, dc_H, dc_O = self.forward(X, stop_vec)
             # evaluate error
             loss = cross_entropy_loss(dc_O, Y)
-            # back prop error
+            # back prop
+
 
 
 
